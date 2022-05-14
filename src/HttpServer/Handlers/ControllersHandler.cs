5dadd2225f3ad2522c4007e3c667181c1bd57499
Asriel <fr2500@protonmail.com>
2022-05-15 01:51:25 +0300

    feat: Added extraction result from task

--- src/HttpServer/Handlers/ControllersHandler.cs
+++ src/HttpServer/Handlers/ControllersHandler.cs
@@ -1,5 +1,7 @@
 ﻿using HttpServer.Helpers;
 using Newtonsoft.Json;
+using System.Collections.Concurrent;
+using System.Linq.Expressions;
 using System.Net;
 using System.Reflection;
 
@@ -8,6 +10,7 @@ namespace HttpServer.Handlers
     public class ControllersHandler : IStreamHandler
     {
         private readonly Dictionary<string, Func<object>> _routes;
+        private readonly ConcurrentDictionary<Type, Func<Task, object>> _extractors = new();
 
         public ControllersHandler(Assembly controllersAssemly)
         {
@@ -97,10 +100,42 @@ namespace HttpServer.Handlers
             {
                 await stream.WriteAsync(buffer, 0, buffer.Length);
             }
+            else if (response is Task task)
+            {
+                await task;
+                var result = ExtractValue(task);
+                await WriteControllerResponseAsync(JsonConvert.SerializeObject(result), stream);
+            }
             else
             {
                 await WriteControllerResponseAsync(JsonConvert.SerializeObject(response), stream);
             }
+        }
+        private object? ExtractValue(Task task)
+        {
+            var taskType = task.GetType();
+            if (!taskType.IsGenericType)
+            {
+                return null;
+            }
+            if (!_extractors.TryGetValue(taskType, out var extractor))
+            {
+                _extractors.TryAdd(taskType, extractor = CreateExtractor(taskType));
+            }
+            return extractor(task);
+        }
+
+        private Func<Task, object> CreateExtractor(Type taskType)
+        {
+            var param = Expression.Parameter(typeof(Task));
+            var result = (Func<Task, object>)Expression.Lambda(
+                typeof(Func<Task, object>), 
+                Expression.Convert(
+                    Expression.Property(
+                        Expression.Convert(param, taskType), "Result"), 
+                    typeof(object)), 
+                param).Compile();
+            return result;
         }
 
     }
