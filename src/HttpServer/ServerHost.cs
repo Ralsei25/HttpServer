dc75ab7704debdab106285dcfa7e57a011028b20
miron <miron@MIRON-IDEA-14>
2022-05-14 17:10:06 +0300

    Add project files.

---
+++ src/HttpServer/ServerHost.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace HttpServer
+{
+    public class ServerHost
+    {
+        private string _answer;
+        public ServerHost(string answer)
+        {
+            _answer = answer;
+        }
+
+        public void Start()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 80);
+            listener.Start();
+
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                using (var stream = client.GetStream())
+                {
+                    using (var reader = new StreamReader(stream))
+                    using (var writer = new StreamWriter(stream))
+                    {
+                        for (string? line = null; line != string.Empty; line = reader.ReadLine())
+                        {
+                            Console.WriteLine(line);
+                        }
+
+                        writer.Write(_answer);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/HttpServer.Console/Program.cs
@@ -0,0 +1,4 @@
+﻿using HttpServer;
+
+ServerHost host = new ServerHost("Hello! I'm test server");
+host.Start();
